d1617e298b12927926de0b03e74f3d0c5840b4fc
MuhammadSaqib-319 <saqibdnp2021@gmail.com>
2025-12-10 14:53:04 -0800

    updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,22 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <>
-      
       <h1>saqib ch</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Et aliquid
+        quaerat quod, corporis exercitationem dignissimos similique, unde ea
+        magnam, tempora accusamus at aperiam provident reiciendis ut natus
+        commodi cum eos!
+      </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
